b2c8792eb718a561e53103ab4a8b2215e4a44f0e
chris241 <chrisleon325@gmail.com>
2019-08-30 10:40:28 +0300

    ajout4

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,12 +5,24 @@ class EmailsController < ApplicationController
 
   def show
     @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js { }
+    end
   end
 
   def new
+  @email = Email.new()
   end
 
   def create
+      @email = Email.create!(object: Faker::Internet.email , body: Faker::Movies::Ghostbusters.quote)
+      respond_to do |format|
+    if @email.save
+      format.html { redirect_to root_path }
+      format.js { }
+    end
+  end
   end
 
   def update
